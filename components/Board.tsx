66cc5652f5f28dc04327ee826c5c292a95b5ffc7
Kat Lea <katarina.lea@globacore.com>
2022-07-20 17:32:18 -0400

    working game

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,5 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Square from "./Square";
+type Player = 'X' | 'O' | 'BOTH' | null;
+
+function calculateWinner(squares: Player[]) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for(let i = 0; i < lines.length; i++){
+        const [a, b, c] = lines[i];
+
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+        }
+    }
+    return null
+}
 
 function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null))
@@ -8,9 +30,9 @@ function Board() {
         Math.round(Math.random() * 1) === 1 ? 'X' : 'O'
     );
 
-    const [winner, setWinner] = useState(null)
+    const [winner, setWinner] = useState<Player>(null)
 
-    function setSquareValue(index) {
+    function setSquareValue(index: number) {
         const newData = squares.map((val, i) => {
             if(i === index){
                 return currentPlayer;
@@ -21,19 +43,40 @@ function Board() {
         setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X')
     }
 
+    function reset() {
+        setSquares(Array(9).fill(null));
+        setWinner(null);
+        setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? 'X' : 'O');
+    }
+
+    useEffect(() => {
+        const w = calculateWinner(squares)
+
+        if(w) {
+            setWinner(w)
+        }
+
+        if(!w && !squares.filter((square) => !square).length){
+            setWinner("BOTH");
+        }
+    });
 
     return (
-    <div>
+        <div>
 
-        <p>Hey {currentPlayer}, it&apos;s your turn</p>
+            {!winner && <p>Hey {currentPlayer}, it&apos;s your turn</p>}
+            {winner && winner !== 'BOTH' && <p>Congrats {winner}!</p>}
+            {winner && winner === 'BOTH' && <p>Tie!</p>}
 
-        <div className="grid">
-            {Array(9).fill(null).map((_, i) => {
-                return <Square winner={winner} key={i} onClick={() => setSquareValue(i)} value={squares[i]} />
-            })}
+            <div className="grid">
+                {Array(9).fill(null).map((_, i) => {
+                    return <Square winner={winner} key={i} onClick={() => setSquareValue(i)} value={squares[i]} />
+                })}
+            </div>
+
+            <button className="reset" onClick={reset}>RESET GAME</button>
+
         </div>
-
-    </div>
     );
 }
 
