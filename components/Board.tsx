1ab029d8704654a0998d202234b06ac9ab1cae40
Kat Lea <katarina.lea@globacore.com>
2022-07-20 14:18:29 -0400

    some functionality and styling

---
+++ components/Board.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import Square from "./Square";
+
+function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null))
+
+    const [currentPlayer, setCurrentPlayer] = useState<'X' | 'O'>(
+        Math.round(Math.random() * 1) === 1 ? 'X' : 'O'
+    );
+
+    const [winner, setWinner] = useState(null)
+
+    function setSquareValue(index) {
+        const newData = squares.map((val, i) => {
+            if(i === index){
+                return currentPlayer;
+            }
+            return val;
+        });
+        setSquares(newData)
+        setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X')
+    }
+
+
+    return (
+    <div>
+
+        <p>Hey {currentPlayer}, it&apos;s your turn</p>
+
+        <div className="grid">
+            {Array(9).fill(null).map((_, i) => {
+                return <Square winner={winner} key={i} onClick={() => setSquareValue(i)} value={squares[i]} />
+            })}
+        </div>
+
+    </div>
+    );
+}
+
+export default Board
