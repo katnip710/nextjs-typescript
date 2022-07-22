cbe1e74008997b8b9a6048d90ddc1aa48e3691a2
Kat Lea <katarina.lea@globacore.com>
2022-07-22 14:56:26 -0400

    styling

--- components/Board.tsx
+++ components/Board.tsx
@@ -74,7 +74,7 @@ function Board() {
                 })}
             </div>
 
-            <button className="reset" onClick={reset}>RESET GAME</button>
+            <button className="reset" onClick={reset}>Reset Game</button>
 
         </div>
     );
